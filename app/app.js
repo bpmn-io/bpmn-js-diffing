47f2f87eb9bb00ff2660cb3e752c7f0c35cf2964
Nico Rehwaldt <git_nikku@nixis.de>
2014-08-13 22:26:21 +0200

    feat(app): add zoom scroll + refactor

--- app/app.js
+++ app/app.js
@@ -1,54 +1,53 @@
-(function(window) {
+(function() {
+
   'use strict';
+
 
   var $ = require('jquery'),
       _ = require('lodash'),
       BpmnViewer = require('bpmn-js'),
-      Diffing = require('bpmn-js-diffing'),
-      viewerOld = new BpmnViewer({ container: '#canvas_old', height: '100%', width: '100%' }),
-      viewerNew = new BpmnViewer({ container: '#canvas_new', height: '100%', width: '100%' });
+      Diffing = require('bpmn-js-diffing');
 
+
+  function createViewer(container) {
+    return new BpmnViewer({ container: container, height: '100%', width: '100%' });
+  }
 
 
   // we use $.ajax to load the diagram.
   // make sure you run the application via web-server (ie. connect (node) or asdf (ruby))
 
-  var loaded = 0;
+  function loadDiagram(diagram, viewer, done) {
+    if (diagram.xml) {
+      return viewer.importXML(diagram.xml, done);
+    }
 
-
-  $.get('../resources/pizza-collaboration/old.bpmn', function(pizzaDiagram) {
-    viewerOld.importXML(pizzaDiagram, function(err) {
-
-      if (!err) {
-        console.log('success!');
-        //viewerOld.get('canvas').zoom('fit-viewport');
-        if (++loaded === 2) {
-          showDiff(viewerOld, viewerNew);
-        }
-
-      } else {
-        console.log('something went wrong:', err);
-      }
+    $.get(diagram.url, function(xml) {
+      viewer.importXML(xml, done);
     });
-  });
+  }
 
-  $.get('../resources/pizza-collaboration/new.bpmn', function(pizzaDiagram) {
-    viewerNew.importXML(pizzaDiagram, function(err) {
 
-      if (!err) {
-        console.log('success!');
-        //viewerNew.get('canvas').zoom('fit-viewport');
-        if (++loaded === 2) {
-          showDiff(viewerOld, viewerNew);
-        }
+  function loadDiagrams(diagramOld, diagramNew, done) {
 
-      } else {
-        console.log('something went wrong:', err);
+    var loading = 2;
+
+    var viewerOld = createViewer(diagramOld.container),
+        viewerNew = createViewer(diagramNew.container);
+
+    function doneLoading(err) {
+      if (err) {
+        return done(err);
       }
 
+      if (--loading === 0) {
+        done(null, viewerOld, viewerNew);
+      }
+    }
 
-    });
-  });
+    loadDiagram(diagramOld, viewerOld, doneLoading);
+    loadDiagram(diagramNew, viewerNew, doneLoading);
+  }
 
 
   function showDiff(viewerOld, viewerNew) {
@@ -57,126 +56,139 @@
 
     console.log (result);
 
-      $.each(result._removed, function(i, obj) {
-        viewerOld.get('elementRegistry').getGraphicsByElement(obj).addClass('elementRemoved');
 
-        var overlays = viewerOld.get('overlays');
-        addMarker (overlays, i, "marker-removed", "&minus;");
+    $.each(result._removed, function(i, obj) {
+      viewerOld.get('elementRegistry').getGraphicsByElement(obj).addClass('elementRemoved');
 
+      var overlays = viewerOld.get('overlays');
+      addMarker (overlays, i, "marker-removed", "&minus;");
+
+    });
+
+
+    $.each(result._added, function(i, obj) {
+      viewerNew.get('elementRegistry').getGraphicsByElement(obj).addClass('elementAdded');
+
+      var overlays = viewerNew.get('overlays');
+      addMarker (overlays, i, "marker-added", "&#43;");
+
+    });
+
+
+    $.each(result._layoutChanged, function(i, obj) {
+      console.log (i);
+      viewerOld.get('elementRegistry').getGraphicsByElement(i).addClass('elementMoved');
+      var overlays = viewerOld.get('overlays');
+      addMarker (overlays, i, "marker-layoutChanged", "&#8680;");
+
+      viewerNew.get('elementRegistry').getGraphicsByElement(i).addClass('elementMoved');
+      var overlays = viewerNew.get('overlays');
+      addMarker (overlays, i, "marker-layoutChanged", "&#8680;");
+
+    });
+
+
+    $.each(result._changed, function(i, obj) {
+      viewerOld.get('elementRegistry').getGraphicsByElement(i).addClass('elementEdited');
+
+      var overlays = viewerOld.get('overlays');
+      addMarker (overlays, i, "marker-changed", "&#9998;");
+
+      viewerNew.get('elementRegistry').getGraphicsByElement(i).addClass('elementEdited');
+      var overlays = viewerNew.get('overlays');
+      addMarker (overlays, i, "marker-changed", "&#9998;");
+
+      var details = '<table ><tr><th>Attribute</th><th>old</th><th>new</th></tr>';
+      $.each(obj.attrs, function(attr, changes) {
+        details = details + '<tr><td>' + attr + '</td><td>' + changes.oldValue + '</td><td>' + changes.newValue + '</td></tr>';
       });
 
-      $.each(result._added, function(i, obj) {
-        viewerNew.get('elementRegistry').getGraphicsByElement(obj).addClass('elementAdded');
+      details = details + '</table></div>';
 
-        var overlays = viewerNew.get('overlays');
-        addMarker (overlays, i, "marker-added", "&#43;");
-
+      viewerOld.get('elementRegistry').getGraphicsByElement(i).click (function (event) {
+        $('#changeDetailsOld_' + i).toggle();
       });
 
-      $.each(result._layoutChanged, function(i, obj) {
-        console.log (i);
-        viewerOld.get('elementRegistry').getGraphicsByElement(i).addClass('elementMoved');
-        var overlays = viewerOld.get('overlays');
-        addMarker (overlays, i, "marker-layoutChanged", "&#8680;");
+      var detailsOld = '<div id="changeDetailsOld_' + i + '" class="changeDetails">' + details;
 
-        viewerNew.get('elementRegistry').getGraphicsByElement(i).addClass('elementMoved');
-        var overlays = viewerNew.get('overlays');
-        addMarker (overlays, i, "marker-layoutChanged", "&#8680;");
+      var overlays = viewerOld.get('overlays');
 
+      // attach an overlay to a node
+      overlays.add(i, {
+        position: {
+          bottom: -5,
+          left: 0
+        },
+        html: detailsOld
       });
 
+      $('#changeDetailsOld_' + i).toggle();
 
-      $.each(result._changed, function(i, obj) {
-          viewerOld.get('elementRegistry').getGraphicsByElement(i).addClass('elementEdited');
-
-          var overlays = viewerOld.get('overlays');
-          addMarker (overlays, i, "marker-changed", "&#9998;");
-
-          viewerNew.get('elementRegistry').getGraphicsByElement(i).addClass('elementEdited');
-          var overlays = viewerNew.get('overlays');
-          addMarker (overlays, i, "marker-changed", "&#9998;");
-
-          var details = '<table ><tr><th>Attribute</th><th>old</th><th>new</th></tr>';
-          $.each(obj.attrs, function(attr, changes) {
-            details = details + '<tr><td>' + attr + '</td><td>' + changes.oldValue + '</td><td>' + changes.newValue + '</td></tr>';
-          });
-
-          details = details + '</table></div>';
-
-          viewerOld.get('elementRegistry').getGraphicsByElement(i).click (function (event) {
-             $('#changeDetailsOld_' + i).toggle();
-          });
-
-          var detailsOld = '<div id="changeDetailsOld_' + i + '" class="changeDetails">' + details;
-
-          var overlays = viewerOld.get('overlays');
-            // attach an overlay to a node
-            overlays.add(i, {
-              position: {
-                bottom: -5,
-                left: 0
-              },
-              html: detailsOld
-            });
-            $('#changeDetailsOld_' + i).toggle();
-
-          viewerNew.get('elementRegistry').getGraphicsByElement(i).click (function (event) {
-             $('#changeDetailsNew_' + i).toggle();
-          });
-
-          viewerNew.get('elementRegistry').getGraphicsByElement(i).addClass('elementEdited');
-
-          var detailsNew = '<div id="changeDetailsNew_' + i + '" class="changeDetails">' + details;
-
-          var overlays = viewerNew.get('overlays');
-            // attach an overlay to a node
-            overlays.add(i, {
-              position: {
-                bottom: -5,
-                left: 0
-              },
-              html: detailsNew
-            });
-            $('#changeDetailsNew_' + i).toggle();
-
-
+      viewerNew.get('elementRegistry').getGraphicsByElement(i).click (function (event) {
+         $('#changeDetailsNew_' + i).toggle();
       });
 
+      viewerNew.get('elementRegistry').getGraphicsByElement(i).addClass('elementEdited');
 
+      var detailsNew = '<div id="changeDetailsNew_' + i + '" class="changeDetails">' + details;
 
+      var overlays = viewerNew.get('overlays');
+
+      // attach an overlay to a node
+      overlays.add(i, {
+        position: {
+          bottom: -5,
+          left: 0
+        },
+        html: detailsNew
+      });
+
+      $('#changeDetailsNew_' + i).toggle();
+    });
   }
 
-  function openDiagram (xml, target) {
-    $( '#' + target ).empty();
 
-    console.log (target);
+  var diagramOld = { url: '../resources/pizza-collaboration/old.bpmn', container: '#canvas_old' },
+      diagramNew = { url: '../resources/pizza-collaboration/new.bpmn', container: '#canvas_new' };
 
-    if (target == "canvas_old") {
-      viewerOld = new BpmnViewer({ container: '#' + target, height: '100%', width: '100%' });
-      viewerOld.importXML(xml, function(err) {
-          if (!err) {
-            console.log('success!');
 
-          } else {
-            console.log('something went wrong:', err);
-          }
-        });
-    } else if (target == "canvas_new") {
-      viewerNew = new BpmnViewer({ container: '#' + target, height: '100%', width: '100%' });
-      viewerNew.importXML(xml, function(err) {
-          if (!err) {
-            console.log('success!');
+  loadDiagrams(diagramOld, diagramNew, function(err, viewerOld, viewerNew) {
 
-          } else {
-            console.log('something went wrong:', err);
-          }
-        });
+    if (err) {
+      return console.log('something went wrong when opening the diagrams', err);
     }
 
+    // show diff
+    showDiff(viewerOld, viewerNew);
+  });
+
+
+
+  var viewerOld, viewerNew;
+
+
+  function openDiagram(xml, target) {
+    var viewer;
+
+    $( '#' + target ).empty();
+
+    viewer = createViewer('#' + target);
+
+    loadDiagram({ xml: xml }, viewer, function(err) {
+      if (err) {
+        console.error('something went wrong:', err);
+      }
+    });
+
+    if (target == 'canvas_old') {
+      viewerOld = viewer;
+    } else if (target == 'canvas_new') {
+      viewerNew = viewer;
+    }
   }
 
   $('.file').on('change', function(e) {
-    openFile(e.target.files[0], openDiagram, $(this).attr('target'));
+    openFile(e.target.files[0], $(this).attr('target'), openDiagram);
   });
 
   $('#diffNow').on('click', function(e) {
@@ -184,15 +196,16 @@
   });
 
 
-  function openFile(file, callback, target) {
-      var reader = new FileReader();
-      reader.onload = function(e) {
-        var xml = e.target.result;
-        callback(xml, target);
-      };
+  function openFile(file, target, done) {
+    var reader = new FileReader();
 
-      reader.readAsText(file);
-    }
+    reader.onload = function(e) {
+      var xml = e.target.result;
+      done(xml, target);
+    };
+
+    reader.readAsText(file);
+  }
 
 
   function addMarker (overlays, elementId, className, symbol) {
@@ -204,36 +217,11 @@
           top: -15,
           right: 20
         },
-        html: "<span class='marker " + className + "'>" + symbol + "</span>"
+        html: '<span class="marker ' + className + '">' + symbol + '</span>'
       });
     } catch (e) {
       // fuck you, haha
     }
-
   }
 
-
-    function oldstuff() {
-
-    var definitions = viewerOld.definitions;
-
-    definitions.$instanceOf('bpmn:FlowNode');
-
-    //debugger;
-
-    _.forEach(definitions.rootElements, function(element) {
-
-      //console.log(element.$type, element);
-      if(element.$instanceOf('bpmn:Process')) {
-        _.forEach(element.flowElements, function(flowElement) {
-          console.log(flowElement.$type, flowElement);
-        });
-      }
-
-
-
-    });
-
-
-    }
-})(window);
+})();
--- app/bpmn.js
+++ app/bpmn.js
@@ -1,6 +1,6 @@
 var BpmnJS = require('bpmn-js/lib/Viewer');
 
-BpmnJS.prototype._modules.push([
+BpmnJS.prototype._modules = BpmnJS.prototype._modules.concat([
   require('bpmn-js/lib/features/movecanvas'),
   require('bpmn-js/lib/features/zoomscroll')
 ]);
